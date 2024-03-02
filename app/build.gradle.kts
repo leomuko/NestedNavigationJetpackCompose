b9f80adadd1e5ad4f586645a83e41f542a461892
leomuko <kezzieleo@gmail.com>
2024-03-02 12:26:40 +0300

    Add navigation and material dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //Navigation Dependency
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material:material:1.6.2")
 }
