9fbbad8a479f1dbbe08b2c075bb6b8e119d4b3b5
leomuko <kezzieleo@gmail.com>
2024-03-02 12:29:27 +0300

    Modify and finish project

--- app/src/main/java/com/example/jetpacknestednavigation/MainActivity.kt
+++ app/src/main/java/com/example/jetpacknestednavigation/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.jetpacknestednavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jetpacknestednavigation.navigation.RootNav
 import com.example.jetpacknestednavigation.ui.theme.JetpackNestedNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +36,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    RootNav()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackNestedNavigationTheme {
-        Greeting("Android")
-    }
-}
